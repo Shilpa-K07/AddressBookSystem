14fabf32d1ce2523b2c017e896c333e7207dccd4
shilpa <shilpa07udupi@gmail.com>
2020-10-28 08:22:54 +0530

    feat: create contacts in addressbook

---
+++ AddressBookSystem.java
@@ -0,0 +1,10 @@
+package com.addresssbooksystem;
+
+import java.util.Scanner;
+
+public class AddressBookSystem {
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to Address Book System");
+	}
+}
