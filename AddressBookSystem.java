f401b30a3ccddc84052377bb2a17b92b8f776e6f
shilpa <shilpa07udupi@gmail.com>
2020-10-28 09:11:59 +0530

    feat: add multiple contact to address book

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -4,8 +4,6 @@ public class AddressBookSystem {
 	public static void main(String[] args)
 	{
 		AddressBookDao addressBook = new AddressBookDao();
-		addressBook.addContact();
-		addressBook.editContact();		
-		addressBook.deleteContact();
+		addressBook.addMultipleContact();
 	}
 }
