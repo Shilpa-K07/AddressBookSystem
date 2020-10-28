0f4764f765abbb62940e2bc35a6f24e70e80b278
shilpa <shilpa07udupi@gmail.com>
2020-10-28 08:55:29 +0530

    feat: edit contact in address book

--- AddressBookDao.java
+++ AddressBookDao.java
@@ -38,4 +38,83 @@ public class AddressBookDao {
 		personMap.put(personName, person);
 		System.out.println("Contact added !!");
 	}
+
+	//UC-3
+	void editContact()
+	{
+		System.out.println("Enter person name to edit: ");
+		String personName = scannerObject.next();
+		PersonEntity person = personMap.get(personName);
+		
+		while(true)
+		{
+			System.out.println("Which field to edit ?");
+			System.out.println("Option 1:Address");
+			System.out.println("Option 2:City");
+			System.out.println("Option 3:State");
+			System.out.println("Option 4:Zip");
+			System.out.println("Option 5:Phone number");
+			System.out.println("Option 6:Email");
+			System.out.println("Option 7:Quit");
+			int option = scannerObject.nextInt();
+			
+			switch(option)
+			{
+				case 1:editAddress(person);
+						break;
+				case 2:editCity(person);
+						break;
+				case 3:editState(person);
+						break;
+				case 4:editZip(person);
+						break;
+				case 5:editPhoneNumber(person);
+						break;
+				case 6:editEmail(person);
+						break;
+				case 7:System.out.println("Exiting..");
+						break;
+				default:System.out.println("Enter numbers between 1 to 7");
+			}
+			if( option == 7)
+				break;
+		}
+	}
+
+	private void editEmail(PersonEntity person) {
+		System.out.println("Enter new email: ");
+		person.setEmail(scannerObject.next());
+		System.out.println("Email edited !!");
+	}
+
+	private void editPhoneNumber(PersonEntity person) {
+		System.out.println("Enter new phone number: ");
+		person.setPhoneNumber(scannerObject.nextLong());
+		System.out.println("Phone number edited !!");
+	}
+
+	private void editZip(PersonEntity person) {
+		System.out.println("Enter new Zip: ");
+		person.setZip(scannerObject.nextLong());
+		System.out.println("Zip edited !!");
+	}
+
+	private void editState(PersonEntity person) {
+		System.out.println("Enter new state: ");
+		person.setState(scannerObject.next());
+		System.out.println("State edited !!");
+	}
+
+	private void editCity(PersonEntity person) {
+		System.out.println("Enter new City: ");
+		person.setCity(scannerObject.next());
+		System.out.println("City edited !!");
+	}
+
+	private void editAddress(PersonEntity person) {
+		System.out.println("Enter new Address: ");
+		person.setAddress(scannerObject.next());
+		System.out.println("Address edited !!");
+	}
+	
 }
--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,11 +1,10 @@
 package com.addresssbooksystem;
 
-import java.util.Scanner;
-
 public class AddressBookSystem {
 	public static void main(String[] args)
 	{
 		AddressBookDao addressBook = new AddressBookDao();
 		addressBook.addContact();
+		addressBook.editContact();		
 	}
 }
